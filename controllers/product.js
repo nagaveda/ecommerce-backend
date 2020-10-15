38f4f39c926e261dbe94732113c676a3e18eded6
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-10-16 01:04:19 +0530

    Added functionality of adding photos to the database

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,7 @@
-const product = require("../models/product");
 const Product = require("../models/product");
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs')
 
 exports.getProductById = (req, res, next, id) => {
     Product.findById(id)
@@ -8,9 +10,62 @@ exports.getProductById = (req, res, next, id) => {
         if(err){
             return res.status(400).json({
                 error: "Product not found"
-            });
+            }); 
         }
         req.product = product;
         next();
     });
+};
+
+exports.createProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+        if(err){
+            return res.status(400).json({
+                error: "Error with the image"
+            });
+        }
+        //Destructure the fields
+        const {name, description, price, category, stock} = fields;
+
+        if (
+            !name||
+            !description ||
+            !price || 
+            !category || 
+            !stock
+        ){
+            return res.json(400).json({
+                error: 'Please include all the fields..'
+            });
+
+        }
+
+        
+         let product = new Product(fields);
+
+         //handle file here
+         if(file.photo){
+             if(file.photo.size > 3000000){
+                 return res.status(400).json({
+                     error: "File size too big"
+                 });
+             }
+             product.photo.data = file.photo.path;
+             product.photo.contentType = file.photo.type;
+
+         }
+
+         //save to the DB
+         product.save((err, product) => {
+             if(err){
+                 return res.status(400).json({
+                     error: "Error saving product to DB"
+                 });
+             }
+             res.json(product);
+         })
+    })
 };
--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 const {getUserById} = require('../controllers/user');
-const {getProductById} = require('../controllers/product');
+const {getProductById, createProduct} = require('../controllers/product');
 
 //params
 router.param("userId", getUserById);
 router.param("productId", getProductById);
 
-//Routed
+//Routes
+router.post("/product/create/:userId",isSignedIn, isAuthenticated, isAdmin, createProduct );
 
 
 module.exports =  router;
